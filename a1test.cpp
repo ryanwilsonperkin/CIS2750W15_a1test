6afbadd0ea7762add3a2b8b79876fe8c90f338b4
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-26 16:26:18 -0500

    SetUp and TearDown temp file used for tests.

--- a1test.cpp
+++ a1test.cpp
@@ -1,18 +1,40 @@
+#include <iostream>
+#include <fstream>
+
 #include "gtest/gtest.h"
 #include "ParameterManager.h"
+
+#define TEST_FILENAME "TEMP"
 
 namespace {
 class ParameterManagerTest : public ::testing::Test {
 protected:
+        std::ofstream test_file;
+
         ParameterManagerTest() {}
-        virtual ~ParameterManagerTest() {}
-        virtual void SetUp() {}
-        virtual void TearDown() {}
+
+        virtual ~ParameterManagerTest()
+        {
+                remove(TEST_FILENAME);
+        }
+
+        virtual void SetUp()
+        {
+                test_file.open(TEST_FILENAME);
+        }
+
+        virtual void TearDown()
+        {
+                test_file.close();
+        }
 };
-TEST_F(ParameterManagerTest, Create) {
+
+TEST_F(ParameterManagerTest, Create)
+{
         ParameterManager pm;
 }
-}
+
+}  /* namespace */
 
 int main(int argc, char **argv) {
         ::testing::InitGoogleTest(&argc, argv);
