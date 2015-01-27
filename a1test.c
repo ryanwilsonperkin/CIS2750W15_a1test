734e62763fddaaf166c3b08200bcf759ab05b9ae
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-27 14:11:17 -0500

    Add test_create_pm_twice

--- a1test.c
+++ a1test.c
@@ -16,6 +16,17 @@ int test_create_pm() {
         }
 }
 
+int test_create_pm_twice() {
+        ParameterManager *pm1, *pm2;
+        if (!(pm1 = PM_create(10))) {
+                return 1;
+        }else if  (!(pm2 = PM_create(10))) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
 void run_test(int (*fn)(), char *fn_name) {
         num_tests++;
         printf("\n[a1test][TEST]: %s\n", fn_name);
@@ -30,6 +41,7 @@ void run_test(int (*fn)(), char *fn_name) {
 
 int main(int argc, char **argv) {
         run_test(test_create_pm, "test_create_pm");
+        run_test(test_create_pm_twice, "test_create_pm_twice");
         printf("\n[a1test] Number of tests run: %d\n", num_tests);
         printf("[a1test] Number of successes: %d\n", num_success);
         printf("[a1test] Number of failures: %d\n", num_fail);
