e30f096d88a8af4819e82ec84a5858a1899ed91a
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-26 20:04:13 -0500

    Basic test case.

--- a1test.c
+++ a1test.c
@@ -1,7 +1,34 @@
+#include <stdio.h>
 #include "ParameterManager.h"
 
 #define TEST_FILENAME "TEMP"
 
+int num_tests = 0;
+int num_fail = 0;
+int num_success = 0;
+
+int test_create_pm() {
+        ParameterManager *pm;
+        if (!(pm = PM_create(10))) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+void run_test(int (*fn)(), char *fn_name) {
+        num_tests++;
+        printf("===TEST: %s\n", fn_name);
+        if ((*fn)()) {
+                num_fail++;
+                printf("===FAILURE: %s\n", fn_name);
+        } else {
+                num_success++;
+                printf("===SUCCESS: %s\n", fn_name);
+        }
+}
+
 int main(int argc, char **argv) {
+        run_test(test_create_pm, "test_basic");
         return 0;
 }
