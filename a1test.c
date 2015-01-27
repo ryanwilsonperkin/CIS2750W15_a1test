7bc285b7b5cc5179438a7badbb236d0b5d98404b
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-27 18:22:35 -0500

    Tests for PM_manage various values.

--- a1test.c
+++ a1test.c
@@ -38,6 +38,106 @@ int test_pm_destroy() {
         }
 }
 
+int test_pm_manage_int_optional() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", INT_TYPE, 0)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_int_required() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", INT_TYPE, 1)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_real_optional() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", REAL_TYPE, 0)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_real_required() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", REAL_TYPE, 1)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_boolean_optional() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", BOOLEAN_TYPE, 0)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_boolean_required() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", BOOLEAN_TYPE, 1)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_string_optional() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", STRING_TYPE, 0)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_string_required() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", STRING_TYPE, 1)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_list_optional() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", LIST_TYPE, 0)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
+int test_pm_manage_list_required() {
+        ParameterManager *pm;
+        pm = PM_create(DEFAULT_CREATE_VAL);
+        if (!PM_manage(pm, "name", LIST_TYPE, 1)) {
+                return 1;
+        } else {
+                return 0;
+        }
+}
+
 void run_test(int (*fn)(), char *fn_name) {
         num_tests++;
         printf("\n[a1test][TEST]: %s\n", fn_name);
@@ -54,6 +154,16 @@ int main(int argc, char **argv) {
         run_test(test_pm_create, "test_pm_create");
         run_test(test_pm_create_twice, "test_pm_create_twice");
         run_test(test_pm_destroy, "test_pm_destroy");
+        run_test(test_pm_manage_int_optional, "test_pm_manage_int_optional");
+        run_test(test_pm_manage_int_required, "test_pm_manage_int_required");
+        run_test(test_pm_manage_real_optional, "test_pm_manage_real_optional");
+        run_test(test_pm_manage_real_required, "test_pm_manage_real_required");
+        run_test(test_pm_manage_boolean_optional, "test_pm_manage_boolean_optional");
+        run_test(test_pm_manage_boolean_required, "test_pm_manage_boolean_required");
+        run_test(test_pm_manage_string_optional, "test_pm_manage_string_optional");
+        run_test(test_pm_manage_string_required, "test_pm_manage_string_required");
+        run_test(test_pm_manage_list_optional, "test_pm_manage_list_optional");
+        run_test(test_pm_manage_list_required, "test_pm_manage_list_required");
         printf("\n[a1test] Number of tests run: %d\n", num_tests);
         printf("[a1test] Number of successes: %d\n", num_success);
         printf("[a1test] Number of failures: %d\n", num_fail);
